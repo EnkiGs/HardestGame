7c52e89b54cc27a6a8d30f884b510b09d91d23e3
EnkiGs <enki.garrigues@free.fr>
2019-02-06 22:38:45 +0100

    first commit

---
+++ app/src/main/java/iut/projet/hardestgame/MainActivity.java
@@ -0,0 +1,33 @@
+package iut.projet.hardestgame;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import java.util.logging.Level;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void showLevels(View view){
+        Intent intent = new Intent(this, LevelsActivity.class);
+        startActivity(intent);
+    }
+
+    public void startGame(View view){
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
+    }
+
+    public void config(View view){
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
+    }
+
+}
